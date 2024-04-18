19c2e9f150ccf154e5f1aed8df8ecd1292d64ae5
Azi0830 <turakulov.t.a@gmail.com>
2024-04-19 02:06:49 +0900

    m

--- index.js
+++ index.js
@@ -83,25 +83,42 @@
 // }
 
 // console.log(name(strs));
-const strs = ["flower", "flow", "flight"];
 
-function longestCommonPrefix(strs) {
-  if (!strs || strs.length === 0) {
-    return "";
+////
+
+// const strs = ["flower", "flow", "flight"];
+
+// function longestCommonPrefix(strs) {
+//   if (!strs || strs.length === 0) {
+//     return "";
+//   }
+
+//   let prefix = strs[0];
+
+//   for (let i = 1; i < strs.length; i++) {
+//     while (strs[i].indexOf(prefix) !== 0) {
+//       prefix = prefix.substring(0, prefix.length - 1);
+//       if (prefix === "") {
+//         return "";
+//       }
+//     }
+//   }
+
+//   return prefix;
+// }
+
+// console.log(longestCommonPrefix(strs));
+
+///
+
+function strStr(haystack, needle) {
+  if (needle === "") {
+    return 0;
   }
 
-  let prefix = strs[0];
+  let index = haystack.indexOf(needle);
 
-  for (let i = 1; i < strs.length; i++) {
-    while (strs[i].indexOf(prefix) !== 0) {
-      prefix = prefix.substring(0, prefix.length - 1);
-      if (prefix === "") {
-        return "";
-      }
-    }
-  }
-
-  return prefix;
+  return index;
 }
 
-console.log(longestCommonPrefix(strs));
+console.log(strStr("sadbutsad", "but"));
